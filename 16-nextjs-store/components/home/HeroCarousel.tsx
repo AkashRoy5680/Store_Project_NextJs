ece2f5632afcbc99569e2cf69f54da62a27a60e1
git config --global user.name Your <you@example.com>
2024-12-08 22:48:23 +0100

    Loading component implemented

--- 16-nextjs-store/components/home/HeroCarousel.tsx
+++ 16-nextjs-store/components/home/HeroCarousel.tsx
@@ -1,7 +1,9 @@
 import hero1 from '@/public/images/hero1.jpg';
 import hero2 from '@/public/images/hero2.jpg';
 import hero3 from '@/public/images/hero3.jpg';
-import hero4 from '@/public/images/hero4.jpg';
+import hero4 from "@/public/images/hero4.jpg";
+
+
 import {
   Carousel,
   CarouselContent,
